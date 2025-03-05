e0a545ff0e5bde299ea52db7623fbb7e17ec40f1
cchuuu <chuyuanz@nycstudents.net>
2025-03-05 12:31:02 -0500

    a

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,26 @@
+<template>
+  <div class="container">
+    <div v-for="(school, index) in satscore" :key="index" :school="school" :id="index + 1">
+      {{ school.school_name }}
+    </div>
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+import SATStats from '@/components/SATStats.vue'
+const satscore = ref([])
+
+async function getSAT() {
+  const response = await fetch('https://data.cityofnewyork.us/resource/f9bf-2cp4.json')
+  let data = await response.json()
+  satscore.value = data
+  console.log(data)
+}
+
+onMounted(() => {
+  getSAT()
+})
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped></style>
